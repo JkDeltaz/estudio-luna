1258e79044e5c0422489b22e358dd37b83d25c76
Heitor Galvão <heitorjunk@gmail.com>
2025-11-07 18:43:55 -0300

    fix: organização do base e rotas do flask

--- src/app.py
+++ src/app.py
@@ -1,28 +1,24 @@
-from flask import Flask, redirect, request, render_template, url_for
-from routes.home import home
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.register_blueprint(home, url_prefix='/home')
-
-
 @app.route('/')
-def index():
-    return redirect(url_for('home.index'))
+def home():
+    return render_template('index.html')
 
 @app.route('/sobrenos')
-def sobre_nos():
-    return render_template('sobrenos.html')
+def sobrenos():
+    return render_template('pages/sobrenos.html')
 
-@app.route('/download')
-def download():
-    return render_template('download.html')
+@app.route('/devlog')
+def devlog():
+    return render_template('pages/devlog.html')
 
 @app.route('/jogos')
 def jogos():
-    return render_template('jogos.html')
+    return render_template('pages/jogos.html')
 
 @app.route('/nelson-fight')
-def nelson_fight():
-    return render_template('nelson-fight.html')
+def nelsonfight():
+    return render_template('pages/nelson-fight.html')
 
--- src/routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Blueprint, render_template
-
-home = Blueprint('home', __name__)
-
-@home.route('/index')
-def index():
-    return render_template('index.html')
