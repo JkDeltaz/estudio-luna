11ce6bf21e40e30e961aad814290915d141c199e
Heitor Galvão <heitorjunk@gmail.com>
2025-11-07 23:24:02 -0300

    feat: adição do sistema inicial de acesso a página de postagem de blogs

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -22,3 +24,18 @@ def jogos():
 def nelsonfight():
     return render_template('pages/nelson-fight.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        with open('static/dados.json', 'r', encoding='utf-8') as f:
+            dados = json.load(f)['users']
+        for info in dados.values():
+            if info['nome'].lower() == usuario.lower():
+                tipo = info['tipo']
+                return render_template('pages/access.html', nome=info['nome'], tipo=tipo)
+        return render_template('index.html', nome=None, tipo=None)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
